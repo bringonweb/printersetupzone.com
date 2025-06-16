4777d6840d9c34856f3a33c37f8a3d99d8869d31
bringonweb <adityagupta@bringon.info>
2025-06-16 20:24:55 +0530

    add the wordpress blog

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -20,10 +20,10 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'blogprinterset');
+define('DB_NAME', 'u635694844_Printersetupzo');
 
 /** Database username */
-define('DB_USER', 'blogprinterset');
+define('DB_USER', 'u635694844_Printersetupzo');
 
 /** Database password */
 define('DB_PASSWORD', 'Blogprintersetupzone.com@123!');
