9444815447f2336af2ca911ad8a3b567867968c5
bringonweb <adityagupta@bringon.info>
2025-10-24 12:52:52 +0530

    Update title from PrinterTouch to PrinterSetupZone

--- disclaimer.php
+++ disclaimer.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PrinterTouch - Policies</title>
+    <title>PrinterSetupZone - Policies</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="css/policys-style.css">
     <link rel="icon" type="image/png" href="/assets/favicon.png">
