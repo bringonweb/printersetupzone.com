ed386ec75d11aaf15a5874054c36ce4706401358
bringonweb <adityagupta@bringon.info>
2026-01-12 20:47:09 +0530

    Update phone number in contact link

--- index.php
+++ index.php
@@ -172,7 +172,7 @@
                 issues resolved today! </p> -->
 
 
-                <a href="tel:+18887683855" style="text-decoration: none; color: #fff;">
+                <a href="tel:+18583460555" style="text-decoration: none; color: #fff;">
                     <button>ENQUIRE NOW</button></a>
         </div>
     </section>
